89e503170e7fea3d33573cab50bd5306594bb897
LittleDitto <ditoanggitac@gmail.com>
2025-01-11 11:08:09 +0700

    commit 2

--- app/db/database.py
+++ app/db/database.py
@@ -1,4 +1,4 @@
 import motor.motor_asyncio
 
-client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
+client = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://ditoanggitac:EAiat6mC5n4MS725@praktikum.dzzicwj.mongodb.net/")
 db = client.duodb
